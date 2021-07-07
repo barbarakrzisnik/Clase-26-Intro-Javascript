5559000861164f3989f7288e1d5d14c8a5a8014e
barbarakrzisnik <barbarakrzisnik3@gmail.com>
2021-07-07 17:17:00 -0300

    intro javascript

---
+++ index.js
@@ -0,0 +1,13 @@
+let nombre = "Barbara"
+let edad = 26
+let codigoPostal = 1754
+
+typeof nombre
+typeof edad
+typeof codigoPostal
+
+let nombreEdad = nombre + edad
+let edadCodigo = edad - codigoPostal
+
+console.log (nombreEdad)
+console.log (edadCodigo)
